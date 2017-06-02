2e9ef18c5e9805a0e44b36c2706633bfc0cea23b
songbinliu <songbin.liu@turbonomic.com>
2017-06-02 16:21:22 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 kubeMaster=http://10.10.174.116:8080
-./reflectorTest --v 4 \
+./KubeReflectorTest --v 4 \
                 --masterUrl $kubeMaster \
                 --alsologtostderr
 
-#./reflectorTest --v 4 \
+#./KubeReflectorTest --v 4 \
 #                --kubeconfig ./configs/aws.kubeconfig.yaml \
 #                --alsologtostderr
